c037f1682d61c056e515c804691af4515cda0abc
Jan van de Locht <jan@vandelocht.uk>
2025-10-30 11:53:28 +0000

    reactivated main loop

--- src/main.cpp
+++ src/main.cpp
@@ -30,48 +30,48 @@ int main() {
 
     // cout << dbManager.calculateDailyHours("2025-10-28").hours << endl;
     dbManager.populateDailyHours();
-    // while (true) {
-    //   cout << "> ";
-    //   getline(cin, input);
+    while (true) {
+        cout << "> ";
+        getline(cin, input);
 
-    //   istringstream iss(input);
-    //   string cmdStr;
-    //   iss >> cmdStr;
+        istringstream iss(input);
+        string cmdStr;
+        iss >> cmdStr;
 
-    //   switch (parseCommand(cmdStr)) {
-    //   case Command::HELP:
-    //     cout << "Available commands: help(h), timestamp(t), quit(q), "
-    //             "writeDB(d), printDB(pd)\n";
-    //     break;
+        switch (parseCommand(cmdStr)) {
+        case Command::HELP:
+            cout << "Available commands: help(h), timestamp(t), quit(q), "
+                    "writeDB(d), printDB(pd)\n";
+            break;
 
-    //   case Command::QUIT:
-    //     return 0;
+        case Command::QUIT:
+            return 0;
 
-    //   case Command::TIMESTAMP:
-    //     if (ts.createTimestamp() != true) {
-    //       cout << "Couldnt create Timestamp!" << endl;
-    //     }
-    //     ts.print();
-    //     break;
+        case Command::TIMESTAMP:
+            if (ts.createTimestamp() != true) {
+                cout << "Couldnt create Timestamp!" << endl;
+            }
+            ts.print();
+            break;
 
-    //   case Command::PRINT:
-    //     ts.print();
-    //     break;
+        case Command::PRINT:
+            ts.print();
+            break;
 
-    //   case Command::PRINT_DB_SHELL:
-    //     dbManager.printTableShell();
-    //     break;
+        case Command::PRINT_DB_SHELL:
+            dbManager.printTableShell();
+            break;
 
-    //   case Command::WRITE_DB:
-    //     dbManager.insertTimestamp(ts.getTimezone(), ts.getFormattedTime(),
-    //                               ts.getTypeString());
-    //     break;
+        case Command::WRITE_DB:
+            dbManager.insertTimestamp(ts.getTimezone(), ts.getFormattedTime(),
+                                      ts.getTypeString());
+            break;
 
-    //   case Command::UNKNOWN:
-    //     cout << "Unknown command. Type 'help' for options.\n";
-    //     break;
-    //   }
-    // }
+        case Command::UNKNOWN:
+            cout << "Unknown command. Type 'help' for options.\n";
+            break;
+        }
+    }
 
     return 0;
 }
